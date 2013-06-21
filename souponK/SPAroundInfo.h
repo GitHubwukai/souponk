fedb736f730b5cc01448ac7e0d4f944d4ba6b81b
wu kai <15837731858@163.com>
2013-06-21 16:51:25 +0800

    添加SPAroundInfo类

---
+++ souponK/SPAroundInfo.h
@@ -0,0 +1,18 @@
+//
+//  SPAroundInfo.h
+//  souponK
+//
+//  Created by wukai on 13-6-21.
+//  Copyright (c) 2013年 wukai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPAroundInfo : NSObject
+@property (nonatomic, retain) NSString *s_id;
+@property (nonatomic, retain) NSString *s_caption;
+@property (nonatomic, retain) NSString *s_description;
+@property (nonatomic, retain) NSString *s_icon;
+@property (nonatomic, retain) NSString *s_address;
+
+@end
---
+++ souponK/SPAroundInfo.m
@@ -0,0 +1,13 @@
+//
+//  SPAroundInfo.m
+//  souponK
+//
+//  Created by wukai on 13-6-21.
+//  Copyright (c) 2013年 wukai. All rights reserved.
+//
+
+#import "SPAroundInfo.h"
+
+@implementation SPAroundInfo
+
+@end
